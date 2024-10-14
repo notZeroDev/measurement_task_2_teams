d3fd7038fdd5528a23b0d143a804fadfed9a2030
Mohamed Ayman <63757748+notZeroDev@users.noreply.github.com>
2024-10-15 00:42:52 +0300

    Initial files

---
+++ script.py
@@ -0,0 +1,69 @@
+import serial
+import time
+
+# configuration variables
+serial_port = 'COM5'  # Change this to your port
+baud_rate = 115200 # change this to your Serial port
+i = 0
+TRIES = 20
+
+# Create a serial connection
+try:
+    ser = serial.Serial(serial_port, baud_rate)
+    time.sleep(2)  # Wait for the connection to establish
+except serial.SerialException:
+    print(f"Could not open serial port {serial_port}. Please check the port.")
+    exit()
+
+def output(line):
+    global i, file, TRIES
+    mode, value = line.split(); # convert the line into mode and value
+    if mode == 's':
+        # tries data
+        print('/'*50)
+        TRIES = int(value)
+        print(f'You Will Read {TRIES} Tries')
+        print('To cahnge the number change the LOOPCOUNT variable int the first line in arduino sketch to the desird number')
+    elif mode == 't':
+        # time data
+        print('/'*50)
+        print(f'The Cycle Time will be: {value} millis')
+        print(f'To Change The Time of Eacg Cycle Change the CYCLE_TIME_MILLIS on The second Line')
+        print('/'*50)
+    elif mode == 'c':
+        # calibraion mode
+        print(f'Calibrating: {value} cm')
+    elif mode == 'r':
+        # recording mode
+        if i == 0:
+            file = open('results.csv', 'w')
+            file.write("i,value\n")
+        i += 1
+        print(f'{i} Recorded: {value} cm')
+        file.write(f"{i},{value}\n")
+
+        # ending of recording data and close the file
+        if i == TRIES:
+            file.close()
+            print('results.csv is done')
+            print('to reset the file press reset button on the board')
+            
+# Function to read data from the serial port
+def read_serial():
+    global file
+    while True:
+        try:
+            if ser.in_waiting > 0:  # Check if data is available to read
+                line = ser.readline().decode('utf-8').rstrip()  # Read a line and decode it
+                output(line)
+        except KeyboardInterrupt:
+            print("Exiting...")
+            file.close()
+            break
+        except Exception as e:
+            print(f"Error: {e}")
+# Call the function
+read_serial()
+
+# Close the serial connection
+ser.close()
---
+++ task_2.ino
@@ -0,0 +1,87 @@
+//configurations variables
+#define LOOP_COUNT 1000
+#define CYCLE_TIME_MILLIS 100
+
+// Define pins for ultrasonic sensor
+const int trigPin = 9;  // Trigger pin
+const int echoPin = 10; // Echo pin
+const int outputLed = 5;
+const int startButton = 6;
+
+// function protorype
+int calcDistance();
+
+
+void setup() {
+  // Initialize serial communication for debugging
+  Serial.begin(115200);
+
+  // Define pin modes for the ultrasonic sensor
+  pinMode(trigPin, OUTPUT); // Trigger pin as output
+  pinMode(echoPin, INPUT);  // Echo pin as input
+  pinMode(outputLed, OUTPUT);
+  pinMode(startButton, INPUT);
+
+
+  // sending comfiguration data to python
+  // sending value of LOOP_COUNT
+  digitalWrite(outputLed, HIGH);
+  Serial.print("s ");
+
+  // sending value of CYCLE_TIME_MILLIS
+  Serial.println(LOOP_COUNT);
+  Serial.print("t ");
+  Serial.println(CYCLE_TIME_MILLIS);
+  delay(1000);
+}
+
+int state = 1; // state is used to make the code runs only once
+
+void loop() {
+  if(state){ // checks if we on the first excutes
+
+  // sending data for calibraion
+  Serial.print("c ");
+  Serial.println(calcDistance());
+
+  // if button is clicked it will start sending data for recording
+  if(digitalRead(startButton)){
+    delay(1000);
+    for(int i = 0; i< LOOP_COUNT;i++){
+    
+      // Print the distance to the serial monitor
+      Serial.print("r ");
+      Serial.println(calcDistance());
+      digitalWrite(outputLed, HIGH);
+      delay(CYCLE_TIME_MILLIS / 2);
+      // Wait for a short while before next measurement
+      
+      digitalWrite(outputLed, LOW);
+      delay(CYCLE_TIME_MILLIS / 2);
+    }
+    
+    state = 0; // end the excution of the program
+    }
+  }
+}
+
+//function define
+int calcDistance(){
+  // Clear the trigger pin
+      digitalWrite(trigPin, LOW);
+      delayMicroseconds(2);
+    
+      // Set the trigger pin HIGH for 10 microseconds to send a pulse
+      digitalWrite(trigPin, HIGH);
+      delayMicroseconds(10);
+      digitalWrite(trigPin, LOW);
+    
+      // Read the echo pin, and calculate the duration the signal took to return
+      long duration = pulseIn(echoPin, HIGH);
+    
+      // Calculate the distance in centimeters
+      // Speed of sound is 343 m/s or 34300 cm/s
+      // Distance = (duration * speed of sound) / 2, because the signal goes and returns
+      long distanceCm = duration * 0.034 / 2;
+      return distanceCm;
+}
